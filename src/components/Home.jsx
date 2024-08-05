2721b3333e4db6843c6d9cd5055260c2f4b6e4aa
KipkorirA <aron.kipkorir@student.moringaschool.com>
2024-08-05 20:41:45 +0300

    Added a form component for users to add new transaction and updated the Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,33 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TransactionList from './TransactionList';
+import TransactionForm from './TransactionForm';
 
 const Home = () => {
   const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/transactions")
+    fetch("http://localhost:3001/transactions")
       .then(res => res.json())
       .then(data => setTransactions(data))
       .catch(error => console.log(error));
   }, []);
 
+  const addTransaction = (newTransaction) => {
+    fetch("http://localhost:3001/transactions", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTransaction),
+    })
+      .then(res => res.json())
+      .then(data => setTransactions([...transactions, data]))
+      .catch(error => console.log(error));
+  };
+
   return (
     <div>
+      <TransactionForm addTransaction={addTransaction} />
       <TransactionList transactions={transactions} />
     </div>
   );
