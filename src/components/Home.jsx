1d72bb58f44b329d40d352ee5018acae2e724a16
KipkorirA <aron.kipkorir@student.moringaschool.com>
2024-08-05 21:22:30 +0300

    Added search component and integrated it with Home Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import TransactionList from './TransactionList';
 import TransactionForm from './TransactionForm';
+import Search from './Search';
 
 const Home = () => {
   const [transactions, setTransactions] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    fetch("http://localhost:3001/transactions")
+    fetch("http://localhost:3000/transactions")
       .then(res => res.json())
       .then(data => setTransactions(data))
       .catch(error => console.log(error));
   }, []);
 
   const addTransaction = (newTransaction) => {
-    fetch("http://localhost:3001/transactions", {
+    fetch("http://localhost:3000/transactions", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -24,10 +26,15 @@ const Home = () => {
       .then(data => setTransactions([...transactions, data]))
       .catch(error => console.log(error));
   };
+  const filteredTransactions = transactions.filter(transaction =>
+    transaction.description.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div>
+        <header>Bank of Flatiron</header>
       <TransactionForm addTransaction={addTransaction} />
+      <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <TransactionList transactions={transactions} />
     </div>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+
+const Search = ({ searchQuery, setSearchQuery }) => {
+  return (
+    <input
+      type="text"
+      placeholder="Search by description..."
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+    />
+  );
+};
+
+export default Search;
